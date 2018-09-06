56f1b40d90ffd8f146e8488cd22b899586ecad50
camktech <camktech@gmail.com>
2018-09-05 21:00:26 -0700

    add support for blank assignee

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -7,21 +7,21 @@ class Issue < ApplicationRecord
   enum status: STATUSES
 
   belongs_to :reporter, class_name: 'User'
-  belongs_to :assignee, class_name: 'User'
+  belongs_to :assignee, class_name: 'User', optional: true
 
-  validates :summary, :reporter_id, :assignee_id, presence: true
+  validates :summary, :reporter_id, presence: true
 
   def self.filter(filters = {})
     page = filters[:page].blank? ? 1 : filters[:page]
     per_page = filters[:per_page].blank? ? DEFAULT_PER_PAGE : filters[:per_page]
     order_by = SORT_OPTIONS[filters[:order_by]].blank? ? DEFAULT_SORT : SORT_OPTIONS[filters[:order_by]]
 
-    where('summary LIKE ?', "%#{filters[:summary]}%").includes(:reporter, :assignee)
-    .joins('INNER JOIN users as reporter ON issues.reporter_id = reporter.id')
-    .joins('INNER JOIN users as assignee ON issues.assignee_id = assignee.id')
-    .where('reporter.name LIKE ?' ,"%#{filters[:reporter_name]}%")
-    .where('assignee.name LIKE ?' ,"%#{filters[:assignee_name]}%")
-    .paginate(page: page, per_page: per_page)
-    .order('created_at' => order_by)
+    filtered_issues = where('summary LIKE ?', "%#{filters[:summary]}%").includes(:reporter, :assignee)
+    filtered_issues = filtered_issues.joins('LEFT JOIN users as reporter ON issues.reporter_id = reporter.id')
+    filtered_issues = filtered_issues.joins('LEFT JOIN users as assignee ON issues.assignee_id = assignee.id')
+    filtered_issues = filtered_issues.where('reporter.name LIKE ?' ,"%#{filters[:reporter_name]}%")
+    filtered_issues = filtered_issues.where('assignee.name LIKE ?' ,"%#{filters[:assignee_name]}%") if filters[:assignee_name].present?
+    filtered_issues = filtered_issues.paginate(page: page, per_page: per_page)
+    filtered_issues.order('created_at' => order_by)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,8 @@ end
   users = User.find(User.pluck(:id).sample(2))
   Issue.create(summary: Faker::Lorem.sentence, status: Issue::STATUSES.sample, reporter_id: users[0].id, assignee_id: users[1].id)
 end
+
+10.times do
+  users = User.find(User.pluck(:id).sample(2))
+  Issue.create(summary: Faker::Lorem.sentence, status: Issue::STATUSES.sample, reporter_id: users[0].id, assignee_id: nil)
+end
